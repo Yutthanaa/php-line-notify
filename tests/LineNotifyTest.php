eee5f06cee7d50f62ac5996ba97f521b9edcf721
Yutthanaa <43489239+Yutthanaa@users.noreply.github.com>
2018-09-22 13:54:44 +0700

    Update LineNotifyTest.php

--- tests/LineNotifyTest.php
+++ tests/LineNotifyTest.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../src/LineNotify.php';
 class LineNotifyTest extends PHPUnit_Framework_TestCase {
 
   private $LineNotify;
-  private $token = 'this_is_a_token';
+  private $token = 'OXbNNfvjYdpeQq6yH1UnriEwikhyaF7f2QHJ5iXTyOR';
 
   public function __construct() {
     $this->LineNotify = new KS\Line\LineNotify($this->token);
